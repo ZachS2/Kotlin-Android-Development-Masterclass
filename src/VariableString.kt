9817b242e430d7952f2d35a51c1c3f8c3b154215
Zach <zrsrenegade2@gmail.com>
2021-08-27 08:01:07 -0400

    Files added up to the date of 8.27

---
+++ src/VariableString.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    //Variables = A placeholder in programming that can hold information and be added to a function, as if a bucket with information.
+    //Types of Variables: String, Int, Double, Boolean, Float, Char
+
+    //For Example:
+
+    var country = "Italy"
+    country = "North America"
+    //Because of the second line redefining the "var" it has taken over
+    println(country)
+}
