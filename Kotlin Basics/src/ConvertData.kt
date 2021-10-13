2cccbc7e02fe3168cd2f97c15b9ba81699e77f45
Zach <zrsrenegade2@gmail.com>
2021-10-13 11:52:13 -0400

    Lots of New files made which have been added

---
+++ Kotlin Basics/src/ConvertData.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    //Converting Data Types
+
+    var age: Int = 3
+    var bloodPressure = 89.0
+    age = bloodPressure.toInt() // integer
+
+    println(age )
+}
---
+++ Kotlin Basics/src/Math.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+
+    //Numbers to Work with
+    var firstnum = 1024
+    var secondnum = 2048
+
+
+    //Addition in Kotlin
+    var resultaddition:Int?
+
+    resultaddition = firstnum + secondnum
+    println("1024 + 2048 = $resultaddition")
+
+    //Subtraction
+    var resultsubtraction:Int?
+
+    resultsubtraction = firstnum - secondnum
+    println("1024 - 2048 = $resultsubtraction")
+
+    //Multiplication
+    var resultmultiply:Int?
+
+    resultmultiply = firstnum * secondnum
+    println("1024 * 2048 = $resultmultiply")
+
+    //Division
+    var resultdivide:Int?
+
+    resultdivide = firstnum / secondnum
+    println("1024 / 2048 = $resultdivide")
+}
